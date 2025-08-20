435ab7204420f9dc749a775dbf8dd6f218f77333
Benson Chen <tumlivein@gmail.com>
2025-08-20 23:33:46 +0800

    feat: 4. GraphQL Server

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,14 @@
-import { execute, parse } from "graphql";
+import { createServer } from "node:http";
+import { createYoga } from "graphql-yoga";
 import { schema } from "./schema";
 
 async function main() {
-  const myQuery = parse(`
-    query {
-      hello
-    }
-  `);
+  const yoga = createYoga({ schema });
+  const server = createServer(yoga);
 
-  const result = await execute({
-    schema,
-    document: myQuery,
+  server.listen(4000, () => {
+    console.info("Server is running on http://localhost:4000/graphql");
   });
-
-  console.log(result);
 }
 
 main();
