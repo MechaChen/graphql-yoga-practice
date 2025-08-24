2f431001c541c75f26e9ac9d805f468cb7d10391
Benson Chen <tumlivein@gmail.com>
2025-08-24 14:01:21 +0800

    feat: 5, 6, 7. Query, Mutation, Adding a Database

--- src/schema.ts
+++ src/schema.ts
@@ -1,14 +1,60 @@
 import { createSchema } from "graphql-yoga";
 
+type Link = {
+  id: string;
+  url: string;
+  description: string;
+};
+
 const typeDefinitions = `
   type Query {
-    hello: String!
+    info: String!
+    feed: [Link!]!
+  }
+  
+  type Mutation {
+    postLink(url: String!, description: String!): Link!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
+const links: Link[] = [
+  {
+    id: "link-0",
+    url: "https://graphql-yoga.com",
+    description: "The easiest way of setting up a GraphQL server",
+  },
+];
+
 const resolvers = {
   Query: {
-    hello: () => "Hello World!",
+    info: () => "This is the API of a Hackernews Clone",
+    feed: () => links,
+  },
+  Mutation: {
+    postLink: (parent: unknown, args: { url: string; description: string }) => {
+      const idCount = links.length;
+
+      const link = {
+        id: `link-${idCount}`,
+        description: args.description,
+        url: args.url,
+      };
+
+      links.push(link);
+
+      return link;
+    },
+  },
+  Link: {
+    id: (parent: Link) => parent.id,
+    description: (parent: Link) => parent.description,
+    url: (parent: Link) => parent.url,
   },
 };
 
